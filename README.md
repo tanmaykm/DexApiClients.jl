7ae6c09c2146052bd9cf44b05e658219bae4bc09
tan <tanmaykm@gmail.com>
2021-05-04 17:01:25 +0530

    update tests

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# DexClient
+# DexApiClients
 
-[![Build Status](https://travis-ci.org/tanmaykm/DexClient.jl.svg?branch=master)](https://travis-ci.org/tanmaykm/DexClient.jl)
-[![Coverage Status](https://coveralls.io/repos/tanmaykm/DexClient.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/tanmaykm/DexClient.jl?branch=master)
-[![codecov.io](http://codecov.io/github/tanmaykm/DexClient.jl/coverage.svg?branch=master)](http://codecov.io/github/tanmaykm/DexClient.jl?branch=master)
+[![Build Status](https://travis-ci.org/tanmaykm/DexApiClients.jl.svg?branch=master)](https://travis-ci.org/tanmaykm/DexApiClients.jl)
+[![Coverage Status](https://coveralls.io/repos/tanmaykm/DexApiClients.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/tanmaykm/DexApiClients.jl?branch=master)
 
 Julia client for [DexIDP (CoreOS) Dex](https://github.com/dexidp/dex)
+
+Example usage:
+
+```julia
+julia> using DexApiClients, DexApiClients.api, gRPCClient
+
+julia> dex = DexBlockingClient("http://127.0.0.1:5557");
+
+julia> out, status = GetVersion(dex, VersionReq());
+
+julia> gRPCCheck(status);
+
+julia> println("API Version: $(out.api)")
+API Version: 2
+```
--- test/testdex.jl
+++ test/testdex.jl
@@ -1,18 +1,24 @@
-using DexClient
+using DexApiClients
+using DexApiClients.api
+using gRPCClient
 using Test
 
 function test_version(dex, api_ver)
-    inp = DexClient.VersionReq()
-    out = DexClient.GetVersion(dex, inp)
-    @test isa(out, DexClient.VersionResp)
+    inp = VersionReq()
+    out, status = GetVersion(dex, inp)
+    gRPCCheck(status)
+
+    @test isa(out, VersionResp)
     @test out.api == api_ver
     nothing
 end
 
 function test_list_passwords(dex, min_npwd, find_user)
-    inp = DexClient.ListPasswordReq()
-    out = DexClient.ListPasswords(dex, inp)
-    @test isa(out, DexClient.ListPasswordResp)
+    inp = ListPasswordReq()
+    out, status = ListPasswords(dex, inp)
+    gRPCCheck(status)
+
+    @test isa(out, ListPasswordResp)
     @test length(out.passwords) >= min_npwd
     if find_user !== nothing
         filtered_passwords = filter(find_user, out.passwords)
@@ -22,37 +28,45 @@ function test_list_passwords(dex, min_npwd, find_user)
 end
 
 function test_update_password(dex, email, new_hash, new_username, expect_not_found)
-    inp = DexClient.UpdatePasswordReq(;email=email, new_hash=new_hash, new_username=new_username)
-    out = DexClient.UpdatePassword(dex, inp)
-    @test isa(out, DexClient.UpdatePasswordResp)
+    inp = UpdatePasswordReq(;email=email, new_hash=new_hash, new_username=new_username)
+    out, status = UpdatePassword(dex, inp)
+    gRPCCheck(status)
+
+    @test isa(out, UpdatePasswordResp)
     @test out.not_found == expect_not_found
     nothing
 end
 
 function test_create_password(dex, email, hash, username, user_id)
-    pwd = DexClient.Password(;email=email, hash=hash, username=username, user_id=user_id)
-    inp = DexClient.CreatePasswordReq(;password=pwd)
-    out = DexClient.CreatePassword(dex, inp)
-    @test isa(out, DexClient.CreatePasswordResp)
+    pwd = Password(;email=email, hash=hash, username=username, user_id=user_id)
+    inp = CreatePasswordReq(;password=pwd)
+    out, status = CreatePassword(dex, inp)
+    gRPCCheck(status)
+
+    @test isa(out, CreatePasswordResp)
     @test !out.already_exists
     nothing
 end
 
 function test_delete_password(dex, email, expect_not_found)
-    inp = DexClient.DeletePasswordReq(;email=email)
-    out = DexClient.DeletePassword(dex, inp)
-    @test isa(out, DexClient.DeletePasswordResp)
+    inp = DeletePasswordReq(;email=email)
+    out, status = DeletePassword(dex, inp)
+    gRPCCheck(status)
+
+    @test isa(out, DeletePasswordResp)
     @test out.not_found == expect_not_found
     nothing
 end
 
 function test_all()
-    dex = DexBlockingClient()
+    dex = DexBlockingClient("http://127.0.0.1:5557"; verbose=false)
 
     test_version(dex, 2)
     test_list_passwords(dex, 1, nothing)
+
     test_update_password(dex, "abc@def.com", Vector{UInt8}("\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"), "new abc", true)
     test_delete_password(dex, "abc@def.com", true)
+
     test_create_password(dex, "abc@def.com", Vector{UInt8}("\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"), "abc", "100")
     test_list_passwords(dex, 2, x->((x.email == "abc@def.com") && (x.username == "abc") && (x.user_id == "100")))
     test_update_password(dex, "abc@def.com", Vector{UInt8}("\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"), "new abc", false)
